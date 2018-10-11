0408642e9949d1a9c7458e8f725f95095195fefa
aashirwadgupta <aash.gupta2@gmail.com>
2018-10-12 01:20:04 +0530

    Making the code more compact and clean

--- src/main/java/com/string/calculator/App.java
+++ src/main/java/com/string/calculator/App.java
@@ -3,35 +3,29 @@ package com.string.calculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-/*import java.util.Arrays;
-import java.util.List;*/
-import java.util.Map;
 import java.util.TreeMap;
 
 import com.string.calculator.service.CalculationEngineService;
 
 public class App 
 {
-	//Commented as not being used now.
-	//private static final List<Character> OPERAND_CHAR_LIST = Arrays.asList('+','-','/','*','^');
-	//private static final List<Character> NUMERICAL_CHAR_LIST = Arrays.asList('0','1','2','3','4','5','6','7','8','9','.');
 	
     public static void main( String[] args )
     {
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
         int testCases = 0;
-        Map<Integer, String> outputMap = new TreeMap<Integer, String>();
+        
         try {
         	testCases = Integer.parseInt(inputReader.readLine());
         	String[] testCasesArr = new String[testCases];
         	for(int i=0; i<testCases; i++) {
         		testCasesArr[i] = inputReader.readLine();
         	}
-        	System.out.println("Validating and calculating Result");
         	CalculationEngineService calculationEngine = new CalculationEngineService();
-        	calculationEngine.startCalculationEngine(testCases, outputMap, testCasesArr);
-        	for(int k=0; k<outputMap.size(); k++) {
-        		System.out.println(testCasesArr[k]+" is "+outputMap.get(k));
+        	TreeMap<Integer, String> resultMap = calculationEngine.startCalculationEngine(testCases, testCasesArr);
+        	for(int insertionOrder : resultMap.keySet()) {
+        		//System.out.println(testCasesArr[k]+" is "+resultMap.get(k));
+        		System.out.println(resultMap.get(insertionOrder));
         	}
         } catch (IOException iOEx) {
         	iOEx.printStackTrace();
